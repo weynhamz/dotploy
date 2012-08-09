05f8784c01bec4e0ba10e1632694d8c357e2b7ee
Techlive Zheng <techlivezheng@gmail.com>
2012-08-09 16:31:13 +0800

    Remove static test structure, set them up on the fly.

--- tests/test-dotploy.sh
+++ tests/test-dotploy.sh
@@ -3,7 +3,81 @@
 USER='techlive'
 HOSTNAME='home'
 
-cd $(dirname ${BASH_SOURCE[0]})
+__test_layer=(
+    '__BACKUP/'
+    '__DOTDIR/.dotdir1/__KEEPED'
+    '__DOTDIR/.dotdir1/subdir/subdirfile'
+    '__DOTDIR/.dotdir1/subfile'
+    '__DOTDIR/.dotdir2/__KEEPED'
+    '__DOTDIR/.dotdir2/subdir/subdirfile'
+    '__DOTDIR/.dotdir2/subfile'
+    '__DOTDIR/.dotdir3/subdir/subdirfile'
+    '__DOTDIR/.dotdir3/subfile'
+    '__DOTDIR/.dotdir4/subdir/subdirfile'
+    '__DOTDIR/.dotdir4/subfile'
+    '__DOTDIR/.dotfile1'
+    '__DOTDIR/.dotfile2'
+    '__DOTDIR/.dotfile3'
+    '__DOTDIR/.dotfile4'
+    '__DOTDIR/.dotfile5'
+    '__DOTDIR/.dotfile6'
+    '__DOTDIR/__HOST.home/.dotdir1/__KEEPED'
+    '__DOTDIR/__HOST.home/.dotdir1/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/.dotdir1/subfile'
+    '__DOTDIR/__HOST.home/.dotdir2/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/.dotdir2/subfile'
+    '__DOTDIR/__HOST.home/.dotdir3/__KEEPED'
+    '__DOTDIR/__HOST.home/.dotdir3/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/.dotdir3/subfile'
+    '__DOTDIR/__HOST.home/.dotdir5/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/.dotdir5/subfile'
+    '__DOTDIR/__HOST.home/.dotfile3'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir1/__KEEPED'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir1/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir1/subfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir2/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir2/subfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir3/__KEEPED'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir3/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir3/subfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir5/subdir/subdirfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotdir5/subfile'
+    '__DOTDIR/__HOST.home/__USER.techlive/.dotfile4'
+    '__DOTDIR/__USER.techlive/.dotdir1/__KEEPED'
+    '__DOTDIR/__USER.techlive/.dotdir1/subdir/subdirfile'
+    '__DOTDIR/__USER.techlive/.dotdir1/subfile'
+    '__DOTDIR/__USER.techlive/.dotdir2/subdir/subdirfile'
+    '__DOTDIR/__USER.techlive/.dotdir2/subfile'
+    '__DOTDIR/__USER.techlive/.dotdir3/__KEEPED'
+    '__DOTDIR/__USER.techlive/.dotdir3/subdir/subdirfile'
+    '__DOTDIR/__USER.techlive/.dotdir3/subfile'
+    '__DOTDIR/__USER.techlive/.dotdir5/subdir/subdirfile'
+    '__DOTDIR/__USER.techlive/.dotdir5/subfile'
+    '__DOTDIR/__USER.techlive/.dotfile2'
+    '__UNUSED/'
+)
+__test_field=$(dirname $0)'/test-field'
+__test_dotsdest=$__test_field'/dotsdest'
+__test_dotsrepo=$__test_field'/dotsrepo'
 
-../dotploy.sh -d dotsrepo .
-../dotploy.sh -r dotsrepo .
+_basedir() {
+    if [[ $1 =~ .*\/$ ]]; then
+        echo $1
+    else
+        dirname $1
+    fi
+}
+
+_test_field() {
+    mkdir -p $__test_dotsdest
+    mkdir -p $__test_dotsrepo
+    for layer in ${__test_layer[@]};do
+        mkdir -p $__test_dotsrepo'/'$(_basedir $layer)
+        [ -d $__test_dotsrepo'/'$(_basedir $layer) ] || touch $layer
+    done
+}
+
+_test_field
+
+$(dirname $0)/../dotploy.sh -d $__test_dotsrepo $__test_dotsdest
+$(dirname $0)/../dotploy.sh -r $__test_dotsrepo $__test_dotsdest
