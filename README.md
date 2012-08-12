46fa5bf4b36cb68d35beab26f2759666bacf064f
Techlive Zheng <techlivezheng@gmail.com>
2012-08-13 03:57:22 +0800

    Use whitespaces for indentation.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Usage
 -----
 
-	dotploy.sh [OPTIONS] PATH_TO_THE_DOTFILES_REPO [DESTINATION_OF_THE_DOT_FILES]
+    dotploy.sh [OPTIONS] PATH_TO_THE_DOTFILES_REPO [DESTINATION_OF_THE_DOT_FILES]
 
-	Options:
-		-r	prune broken symlinks according to the last dotploy.log
-		-d	deploy dotfiles
+    Options:
+        -r  prune broken symlinks according to the last dotploy.log
+        -d  deploy dotfiles
 
 DESTINATION_OF_THE_DOT_FILES is optional, if absent, current $HOME will be used.
 
@@ -26,117 +26,117 @@ Dotfils Repo Structure
 
 The dot files structure must be keepd as same as they are in the original location.
 
-	DOTFILES_REPO
-		|
-		|--------__BACKUP
-		|		|
-		|		|--------$HOSTNAME
-		|		|		|
-		|		|		|--------2012.03.12.14.05.03
-		|		|		|        ^^^^^^^^^^^^^^^^^^^
-		|		|		|        this is the backup directory of the conflict files
-		|		|		|        while deploying.
-		|		|		|
-		|		|		|--------......
-		|
-		|--------__UNUSED
-		|        ^^^^^^^^
-		|        this directory is no usesd, intend to place the dot files that are
-		|        nolonger used, but may be need oneday.
-		|
-		|--------__DOTDIR
-		|		|
-		|		|--------__HOST.$HOSTNAME
-		|		|		|
-		|		|		|--------__USER.$USER
-		|		|		|		|
-		|		|		|		|--------.dotfile1
-		|		|		|		|
-		|		|		|		|--------.dotfile2
-		|		|		|		|
-		|		|		|		|--------......
-		|		|		|		|
-		|		|		|		|--------.dotdir1
-		|		|		|		|		|
-		|		|		|		|		|--------__KEEPED
-		|		|		|		|		         ^^^^^^^^
-		|		|		|		|		         this means this directory shoulde not be deployed, instead,
-		|		|		|		|		         deploy its contents to the corresponding path under $HOME.
-		|		|		|		|
-		|		|		|		|--------.dotdir2
-		|		|		|		|
-		|		|		|		|--------......
-		|		|		|
-		|		|		|
-		|		|		|--------.dotfile1
-		|		|		|
-		|		|		|--------.dotfile2
-		|		|		|
-		|		|		|--------......
-		|		|		|
-		|		|		|--------.dotdir1
-		|		|		|		|
-		|		|		|		|--------__KEEPED
-		|		|		|		         ^^^^^^^^
-		|		|		|		         this means this directory shoulde not be deployed, instead,
-		|		|		|		         deploy its contents to the corresponding path under $HOME.
-		|		|		|
-		|		|		|--------.dotdir2
-		|		|		|
-		|		|		|--------......
-		|		|
-		|		|--------__USER.$USER
-		|		|		|
-		|		|		|--------.dotfile1
-		|		|		|
-		|		|		|--------.dotfile2
-		|		|		|
-		|		|		|--------......
-		|		|		|
-		|		|		|--------.dotdir1
-		|		|		|		|
-		|		|		|		|--------__KEEPED
-		|		|		|		         ^^^^^^^^
-		|		|		|		         this means this directory shoulde not be deployed, instead,
-		|		|		|		         deploy its contents to the corresponding path under $HOME.
-		|		|		|
-		|		|		|--------.dotdir2
-		|		|		|
-		|		|		|--------......
-		|		|
-		|		|--------.dotfile1
-		|		|
-		|		|--------.dotfile2
-		|		|
-		|		|--------......
-		|		|
-		|		|--------.dotdir1
-		|		|		|
-		|		|		|--------__KEEPED
-		|		|		         ^^^^^^^^
-		|		|		         this means this directory shoulde not be deployed, instead,
-		|		|		         deploy its contents to the corresponding path under $HOME.
-		|		|
-		|		|--------.dotdir2
-		|		|
-		|		|--------......
-		|
-		|--------.dotfile1
-		|
-		|--------.dotfile2
-		|
-		|--------......
-		|
-		|--------.dotdir1
-		|		|
-		|		|--------__KEEPED
-		|		         ^^^^^^^^
-		|		         this means this directory shoulde not be deployed, instead,
-		|		         deploy its contents to the corresponding path under $HOME.
-		|
-		|--------.dotdir2
-		|
-		|--------......
+    DOTFILES_REPO
+        |
+        |--------__BACKUP
+        |       |
+        |       |--------$HOSTNAME
+        |       |       |
+        |       |       |--------2012.03.12.14.05.03
+        |       |       |        ^^^^^^^^^^^^^^^^^^^
+        |       |       |        this is the backup directory of the conflict files
+        |       |       |        while deploying.
+        |       |       |
+        |       |       |--------......
+        |
+        |--------__UNUSED
+        |        ^^^^^^^^
+        |        this directory is no usesd, intend to place the dot files that are
+        |        nolonger used, but may be need oneday.
+        |
+        |--------__DOTDIR
+        |       |
+        |       |--------__HOST.$HOSTNAME
+        |       |       |
+        |       |       |--------__USER.$USER
+        |       |       |       |
+        |       |       |       |--------.dotfile1
+        |       |       |       |
+        |       |       |       |--------.dotfile2
+        |       |       |       |
+        |       |       |       |--------......
+        |       |       |       |
+        |       |       |       |--------.dotdir1
+        |       |       |       |       |
+        |       |       |       |       |--------__KEEPED
+        |       |       |       |                ^^^^^^^^
+        |       |       |       |                this means this directory shoulde not be deployed, instead,
+        |       |       |       |                deploy its contents to the corresponding path under $HOME.
+        |       |       |       |
+        |       |       |       |--------.dotdir2
+        |       |       |       |
+        |       |       |       |--------......
+        |       |       |
+        |       |       |
+        |       |       |--------.dotfile1
+        |       |       |
+        |       |       |--------.dotfile2
+        |       |       |
+        |       |       |--------......
+        |       |       |
+        |       |       |--------.dotdir1
+        |       |       |       |
+        |       |       |       |--------__KEEPED
+        |       |       |                ^^^^^^^^
+        |       |       |                this means this directory shoulde not be deployed, instead,
+        |       |       |                deploy its contents to the corresponding path under $HOME.
+        |       |       |
+        |       |       |--------.dotdir2
+        |       |       |
+        |       |       |--------......
+        |       |
+        |       |--------__USER.$USER
+        |       |       |
+        |       |       |--------.dotfile1
+        |       |       |
+        |       |       |--------.dotfile2
+        |       |       |
+        |       |       |--------......
+        |       |       |
+        |       |       |--------.dotdir1
+        |       |       |       |
+        |       |       |       |--------__KEEPED
+        |       |       |                ^^^^^^^^
+        |       |       |                this means this directory shoulde not be deployed, instead,
+        |       |       |                deploy its contents to the corresponding path under $HOME.
+        |       |       |
+        |       |       |--------.dotdir2
+        |       |       |
+        |       |       |--------......
+        |       |
+        |       |--------.dotfile1
+        |       |
+        |       |--------.dotfile2
+        |       |
+        |       |--------......
+        |       |
+        |       |--------.dotdir1
+        |       |       |
+        |       |       |--------__KEEPED
+        |       |                ^^^^^^^^
+        |       |                this means this directory shoulde not be deployed, instead,
+        |       |                deploy its contents to the corresponding path under $HOME.
+        |       |
+        |       |--------.dotdir2
+        |       |
+        |       |--------......
+        |
+        |--------.dotfile1
+        |
+        |--------.dotfile2
+        |
+        |--------......
+        |
+        |--------.dotdir1
+        |       |
+        |       |--------__KEEPED
+        |                ^^^^^^^^
+        |                this means this directory shoulde not be deployed, instead,
+        |                deploy its contents to the corresponding path under $HOME.
+        |
+        |--------.dotdir2
+        |
+        |--------......
 
 Copyright
 ---------
--- dotploy.sh
+++ dotploy.sh
@@ -5,11 +5,11 @@
 # Author: Techliv Zheng <techlivezheng at gmail.com>
 #
 # Usage:
-# 	dotploy.sh [OPTIONS] PATH_TO_THE_DOTFILES_REPO [DESTINATION_OF_THE_DOT_FILES]
+#   dotploy.sh [OPTIONS] PATH_TO_THE_DOTFILES_REPO [DESTINATION_OF_THE_DOT_FILES]
 #
 # Options:
-# 	-r	prune broken symlinks according to the last dotploy.log
-# 	-d	deploy dotfiles
+#   -r  prune broken symlinks according to the last dotploy.log
+#   -d  deploy dotfiles
 #
 # This is a bash only script designed to help easy the $HOME dot files deployment
 # acrossing several hosts. All the hosts share some common dot files. Host specific
@@ -44,18 +44,18 @@ DEPLOY=0
 
 while getopts ":rd" optname
 do
-	case "$optname" in
-		"r")
-			PRUNE=1
-			;;
-		"d")
-			PRUNE=1
-			DEPLOY=1
-			;;
-		"?")
-			echo "Unknown option $OPTARG"
-			;;
-	esac
+    case "$optname" in
+        "r")
+            PRUNE=1
+            ;;
+        "d")
+            PRUNE=1
+            DEPLOY=1
+            ;;
+        "?")
+            echo "Unknown option $OPTARG"
+            ;;
+    esac
 done
 
 shift $((OPTIND - 1))
@@ -75,12 +75,12 @@ BACKUP=$DOTSHOME/__BACKUP/$HOSTNAME/`date +%Y%m%d.%H.%M.%S`
 
 # preserved files
 IGNORE=(
-	"^__USER"
-	"^__HOST"
-	"^__KEEPED"
-	"^__IGNORE"
-	"^.git$"
-	".swp$"
+    "^__USER"
+    "^__HOST"
+    "^__KEEPED"
+    "^__IGNORE"
+    "^.git$"
+    ".swp$"
 )
 
 #
@@ -89,20 +89,20 @@ IGNORE=(
 # Remove broken symlink according to the last dotploy.log
 #
 # Parameters:
-#	$1	dotploy log file
+#   $1  dotploy log file
 #
 #
 doprune() {
-	local logfile=$1
+    local logfile=$1
 
-	local file
-	for file in $(cat $logfile); do
-		docheck $file
+    local file
+    for file in $(cat $logfile); do
+        docheck $file
 
-		[ $? -eq 1 ] && rm -v $file
+        [ $? -eq 1 ] && rm -v $file
 
-		[ $DEPLOY -ne 1 ] && [ -e $file ] && echo $file >> $BACKUP/dotploy.log
-	done
+        [ $DEPLOY -ne 1 ] && [ -e $file ] && echo $file >> $BACKUP/dotploy.log
+    done
 }
 
 #
@@ -111,53 +111,53 @@ doprune() {
 # Check file status.
 #
 # Parameters:
-#	$1	source directory where dotfiles located
-#	$2	destination directory of the dotfiles
-#	$3	filename of the dotfile
+#   $1  source directory where dotfiles located
+#   $2  destination directory of the dotfiles
+#   $3  filename of the dotfile
 #
 docheck() {
-	local src
-	local dst=$1
-	local repath
+    local src
+    local dst=$1
+    local repath
 
-	repath=${dst#$DESTHOME}
-	repath=${repath#/}
+    repath=${dst#$DESTHOME}
+    repath=${repath#/}
 
-	[ -e $DOTSREPO/$repath ] && src=$DOTSREPO/$repath
-	[ -e $DOTSREPO/__USER.$USER/$repath ] && src=$DOTSREPO/__USER.$USER/$repath
-	[ -e $DOTSREPO/__HOST.$HOSTNAME/$repath ] && src=$DOTSREPO/__HOST.$HOSTNAME/$repath
-	[ -e $DOTSREPO/__HOST.$HOSTNAME/__USER.$USER/$repath ] && src=$DOTSREPO/__HOST.$HOSTNAME/__USER.$USER/$repath
+    [ -e $DOTSREPO/$repath ] && src=$DOTSREPO/$repath
+    [ -e $DOTSREPO/__USER.$USER/$repath ] && src=$DOTSREPO/__USER.$USER/$repath
+    [ -e $DOTSREPO/__HOST.$HOSTNAME/$repath ] && src=$DOTSREPO/__HOST.$HOSTNAME/$repath
+    [ -e $DOTSREPO/__HOST.$HOSTNAME/__USER.$USER/$repath ] && src=$DOTSREPO/__HOST.$HOSTNAME/__USER.$USER/$repath
 
-	echo "CHECKING: $dst"
+    echo "CHECKING: $dst"
 
-	if [ -h $dst ];then
-	    local csrc=$(readlink -fm $dst)
+    if [ -h $dst ];then
+        local csrc=$(readlink -fm $dst)
 
-		if [[ $csrc =~ $DOTSHOME ]];then #whether link to dotsrepo
-			if [ "$csrc" == "$src" ];then
-				#all good
-				return 0
-			else
-				#need update
-				return 1
-			fi
-		else
-			#need backup
-			return 2
-		fi
-	elif [ -d $dst ];then
-		[ -f $src/__KEEPED ] && \
-			#if dst is a dir,should check whether it contains high lever files
-			return 4 || \
-			#need backup
-			return 2
-	elif [ -f $dst ];then
-		#need backup
-		return 2
-	else
-		#not existed, do link
-		return 3
-	fi
+        if [[ $csrc =~ $DOTSHOME ]];then #whether link to dotsrepo
+            if [ "$csrc" == "$src" ];then
+                #all good
+                return 0
+            else
+                #need update
+                return 1
+            fi
+        else
+            #need backup
+            return 2
+        fi
+    elif [ -d $dst ];then
+        [ -f $src/__KEEPED ] && \
+            #if dst is a dir,should check whether it contains high lever files
+            return 4 || \
+            #need backup
+            return 2
+    elif [ -f $dst ];then
+        #need backup
+        return 2
+    else
+        #not existed, do link
+        return 3
+    fi
 }
 
 #
@@ -166,72 +166,72 @@ docheck() {
 # Deploy files
 #
 # Parameters:
-#	$1	directory containing files to be deployed
-#	$2	directory where files need to go
+#   $1  directory containing files to be deployed
+#   $2  directory where files need to go
 #
 # This function can be recursive called.
 #
 dodeploy() {
-	local dotdir=$1
-	local dstdir=$2
+    local dotdir=$1
+    local dstdir=$2
 
-	# need to check the src,make sure it is a direcotry
-	docheck $dstdir
+    # need to check the src,make sure it is a direcotry
+    docheck $dstdir
 
-	local status=$?
+    local status=$?
 
-	[ $status -eq 0 ] && return
+    [ $status -eq 0 ] && return
 
-	[ $status -eq 1 ] && rm -v $dstdir
+    [ $status -eq 1 ] && rm -v $dstdir
 
-	# host user based dotfies deploy
-	[ -e $dotdir/__HOST.$HOSTNAME/__USER.$USER ] && \
-		dodeploy $dotdir/__HOST.$HOSTNAME/__USER.$USER $dstdir
+    # host user based dotfies deploy
+    [ -e $dotdir/__HOST.$HOSTNAME/__USER.$USER ] && \
+        dodeploy $dotdir/__HOST.$HOSTNAME/__USER.$USER $dstdir
 
-	# host based dotfies deploy
-	[ -e $dotdir/__HOST.$HOSTNAME ] && \
-		dodeploy $dotdir/__HOST.$HOSTNAME $dstdir
+    # host based dotfies deploy
+    [ -e $dotdir/__HOST.$HOSTNAME ] && \
+        dodeploy $dotdir/__HOST.$HOSTNAME $dstdir
 
-	# user based dotfies deploy
-	[ -e $dotdir/__USER.$USER ] && \
-		dodeploy $dotdir/__USER.$USER $dstdir
+    # user based dotfies deploy
+    [ -e $dotdir/__USER.$USER ] && \
+        dodeploy $dotdir/__USER.$USER $dstdir
 
-	# recursive identifier
-	echo -e "--------\n$dotdir\n--------"
+    # recursive identifier
+    echo -e "--------\n$dotdir\n--------"
 
-	local filelist=$(ls -1A --color=none $dotdir)
+    local filelist=$(ls -1A --color=none $dotdir)
 
-	local file
-	for file in $filelist; do
-		# skip preserved filenames
-		local line
-		for line in ${IGNORE[@]};do
-			[[ $file =~ $line ]] && continue 2
-		done
-		if [ -f $dotdir/__IGNORE ]; then
-			for line in $(cat $dotdir/__IGNORE);do
-				[[ $file =~ $line ]] && continue 2
-			done
-		fi
+    local file
+    for file in $filelist; do
+        # skip preserved filenames
+        local line
+        for line in ${IGNORE[@]};do
+            [[ $file =~ $line ]] && continue 2
+        done
+        if [ -f $dotdir/__IGNORE ]; then
+            for line in $(cat $dotdir/__IGNORE);do
+                [[ $file =~ $line ]] && continue 2
+            done
+        fi
 
-		if [ -d $dotdir/$file ]; then
-			if [ -e $dotdir/$file/__KEEPED ];then
-				# this is a directory needed to be keeped,
-				# deploy its content.
-				dodeploy $dotdir/$file $dstdir/$file
-				# recursive identifier
-				echo -e "--------\n$dotdir\n--------"
-			else
-				dosymlink $dotdir $dstdir $file
-			fi
-		elif [ -f $dotdir/$file ]; then
-			dosymlink $dotdir $dstdir $file
-		fi
+        if [ -d $dotdir/$file ]; then
+            if [ -e $dotdir/$file/__KEEPED ];then
+                # this is a directory needed to be keeped,
+                # deploy its content.
+                dodeploy $dotdir/$file $dstdir/$file
+                # recursive identifier
+                echo -e "--------\n$dotdir\n--------"
+            else
+                dosymlink $dotdir $dstdir $file
+            fi
+        elif [ -f $dotdir/$file ]; then
+            dosymlink $dotdir $dstdir $file
+        fi
 
-		grep "^$dstdir/$file\$" $BACKUP/dotploy.log >/dev/null 2>&1
+        grep "^$dstdir/$file\$" $BACKUP/dotploy.log >/dev/null 2>&1
 
-		[ $? -ne 0 ] && echo "$dstdir/$file" >> $BACKUP/dotploy.log
-	done
+        [ $? -ne 0 ] && echo "$dstdir/$file" >> $BACKUP/dotploy.log
+    done
 }
 
 #
@@ -241,44 +241,44 @@ dodeploy() {
 # If destination file existed, backup first.
 #
 # Parameters:
-#	$1	source directory where dotfiles located
-#	$2	destination directory of the dotfiles
-#	$3	filename of the dotfile
+#   $1  source directory where dotfiles located
+#   $2  destination directory of the dotfiles
+#   $3  filename of the dotfile
 #
 dosymlink() {
-	local src=$1/$3
-	local dst=$2/$3
+    local src=$1/$3
+    local dst=$2/$3
 
-	local repath
+    local repath
 
-	repath=${1#$DOTSREPO}
-	repath=${repath#/}
-	repath=${repath#__HOST.$HOSTNAME}
-	repath=${repath#/}
-	repath=${repath#__USER.$USER}
-	repath=${repath#/}
+    repath=${1#$DOTSREPO}
+    repath=${repath#/}
+    repath=${repath#__HOST.$HOSTNAME}
+    repath=${repath#/}
+    repath=${repath#__USER.$USER}
+    repath=${repath#/}
 
-	# for nested path, need to mkdir parent first
-	[ -n "$repath" ] && mkdir -vp $DESTHOME/$repath
+    # for nested path, need to mkdir parent first
+    [ -n "$repath" ] && mkdir -vp $DESTHOME/$repath
 
-	docheck $dst
+    docheck $dst
 
-	local status=$?
+    local status=$?
 
-	[ $status -eq 1 ] && \
-		rm -v $dst
+    [ $status -eq 1 ] && \
+        rm -v $dst
 
-	# backup existed file
-	[ $status -eq 2 ] && \
-		echo -en "BACKUP:\t" && \
-		local backup=$BACKUP/$repath && \
-		mkdir -vp $backup && \
-		mv -v $dst $backup
+    # backup existed file
+    [ $status -eq 2 ] && \
+        echo -en "BACKUP:\t" && \
+        local backup=$BACKUP/$repath && \
+        mkdir -vp $backup && \
+        mv -v $dst $backup
 
-	# Symlink
-	[ $status -ne 0 ] && [ $status -ne 4 ] && \
-		echo -en "SYMLINK:\t" && \
-		ln -v -s $src $dst
+    # Symlink
+    [ $status -ne 0 ] && [ $status -ne 4 ] && \
+        echo -en "SYMLINK:\t" && \
+        ln -v -s $src $dst
 }
 
 mkdir -vp $BACKUP || exit 1
@@ -288,13 +288,13 @@ echo $DESTHOME > $BACKUP/DESTHOME
 touch $BACKUP/dotploy.log
 
 if [ $PRUNE -eq 1 ];then
-	for logpath in $(grep -l "^$DESTHOME\$" $DOTSHOME/__BACKUP/$HOSTNAME/*/DESTHOME | tail -2 | sed 's-/DESTHOME$--g');do
-		[ "$logpath" = "$BACKUP" ] && continue
+    for logpath in $(grep -l "^$DESTHOME\$" $DOTSHOME/__BACKUP/$HOSTNAME/*/DESTHOME | tail -2 | sed 's-/DESTHOME$--g');do
+        [ "$logpath" = "$BACKUP" ] && continue
 
-		[ -f $logfile ] && doprune $logpath/dotploy.log
-	done
+        [ -f $logfile ] && doprune $logpath/dotploy.log
+    done
 fi
 
 if [ $DEPLOY -eq 1 ];then
-	dodeploy $DOTSREPO $DESTHOME
+    dodeploy $DOTSREPO $DESTHOME
 fi
